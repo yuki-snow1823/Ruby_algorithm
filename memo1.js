a4f7635c40135ab113dc67174f27d592b2dc19b9
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-12 21:32:07 +0900

    [add]js 繰り返されるデータの取得

--- memo1.js
+++ memo1.js
@@ -1,3 +1,18 @@
 split(" ").map(s => parseInt(s))
 
 // 配列を数字にして半角で切り分けて入れる方法
+
+// 繰り返し取得する方法
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  for (let i = 1; i < 5; i++) {
+    console.log(lines[i] - lines[i - 1]);
+  }
+});
