498d1433dca8ff20e6596289eb62f7a9fd71e523
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-17 00:00:21 +0900

    [add]ページネーション　修正

--- memo18.rb
+++ memo18.rb
@@ -10,18 +10,19 @@ mod = div_and_mod[1]
 # 最初のページに全部出す場合
 if all_pages == show_pages
     if select_page == 1
-        puts (1..all_pages).to_a
+        puts (1..all_pages).to_a.join(" ")
     else
         puts "none"
     end
 # 最後のページを聞かれた場合
+elsif div + 1 == select_page  && select_page > all_pages
+    puts "none"
 elsif div + 1 == select_page
     puts (show_pages*(select_page-1)+1..all_pages).to_a.join(" ")
-    
 elsif div + 1  < select_page
     puts "none"
 else
     start = show_pages * select_page - show_pages + 1
     last = show_pages * select_page
-    p (start..last).to_a.join(" ")
+    puts (start..last).to_a.join(" ")
 end
