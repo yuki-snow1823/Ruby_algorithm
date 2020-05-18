bc3cce555eaf0feac402f2b23c22de7ccebcabf6
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-18 21:54:08 +0900

    [add]共通部分の描画

--- memo21.rb
+++ memo21.rb
@@ -19,3 +19,16 @@ arr.each do |f|
     answer << number
 end
 p answer.sum
+
+
+# 共通部分の描画
+start_data = gets.split(" ").map(&:to_i)
+finish_data = gets.split(" ").map(&:to_i)
+
+if finish_data[0] >= 0 && finish_data[1] >= 0
+p start_data[0] * finish_data[0] + start_data[1] * finish_data[1] - (finish_data[0] * finish_data[1])
+elsif finish_data[0] > 0 && finish_data[1] < 0
+p start_data[0] * start_data[1] - ( (start_data[0] - finish_data[0]) * (start_data[1] + finish_data[1]) )
+elsif finish_data[0] < 0 && finish_data[1] > 0
+p start_data[1] * start_data[0] - ( (start_data[1] - finish_data[1]) * (start_data[0] + finish_data[0]) )
+end
