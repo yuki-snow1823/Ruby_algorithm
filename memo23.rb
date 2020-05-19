8fb08bec285d4a2e07d5f551e27a2ffbc828f02a
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-19 22:42:47 +0900

    [add]文字と数字が混ざる配列の仕分け

--- memo23.rb
+++ memo23.rb
@@ -12,6 +12,23 @@ else
     puts "draw"
 end
 
+# ジャンケン
 numbers = gets.split(" ").map(&:to_i)
 
 (1..gets.to_i).map{gets.split(" ").map(&:to_i)}.each {|f| p (f & numbers).count}
+
+data = (1..gets.to_i).map{gets.chomp.split(" ")}
+
+# 文字と数字が混ざっている配列の仕分け
+count = 0 
+data.each do |f|
+    if f[0] == "l"
+      f.map!(&:to_i)
+      count += 1 if f.sum >= 350 && f[4] + f[5] >= 160
+    else
+      f.map!(&:to_i)
+      count += 1 if f.sum >= 350 && f[2] + f[3] >= 160
+    end
+end
+
+p count
