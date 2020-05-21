c6b92bdda08cf6962f2b42a0befff042867c1cf0
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-21 23:19:53 +0900

    [add]給与計算（未解決・要復習）

--- memo26.rb
+++ memo26.rb
@@ -12,3 +12,26 @@
         puts "No Pair"
     end
 end
+
+
+# 給与計算
+data = gets.split(" ").map(&:to_i)
+normal = data[0]
+night = data[1]
+midnight = data[2]
+
+day = gets.to_i
+
+work = (1..day).map{gets.chomp.split(" ").map(&:to_i)} 
+
+work.each do |s,e|
+    if (9..16).to_a.include?(s) && e - s <= 9 
+        p normal * (e - s) 
+    elsif (9..16).to_a.include?(s) && e - s >= 10 && e - s <= 15
+        p 8000 + night (e - s - 8)
+    elsif (17..21).to_a.include?(s) && e -s <= 4
+        p night * (e - s) 
+    else
+        p midnight * (e - s) 
+    end
+end
