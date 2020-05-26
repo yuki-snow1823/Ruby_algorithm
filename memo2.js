c2deebdf4646bd4476c4e43d51c833bec956d8fc
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-26 22:41:19 +0900

    [add]少数の計算

--- memo2.js
+++ memo2.js
@@ -31,3 +31,21 @@ reader.on("close", () => {
   a = lines[0].split(" ");
   a.forEach((element) => console.log(element));
 });
+
+
+// 少数
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+var lines = [];
+var reader = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+reader.on("line", (line) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  a = lines[0].split(" ").map(Number);
+  console.log(a[0] * (100 - a[1]) * 0.01 * (100 - a[2]) * 0.01);
+});
