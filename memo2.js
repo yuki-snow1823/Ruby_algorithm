627151dbe78b01ea99cf9239ae2be691960c739f
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-26 12:04:45 +0900

    [add]カンマ区切りのデータの取得

--- memo2.js
+++ memo2.js
@@ -1,7 +1,5 @@
 process.stdin.resume();
 process.stdin.setEncoding("utf8");
-// 自分の得意な言語で
-// Let's チャレンジ！！
 
 var lines = [];
 var reader = require("readline").createInterface({
@@ -15,4 +13,21 @@ reader.on("close", () => {
   for (let i = 0; i < lines[0]; i++) {
     console.log(lines[i + 1]);
   }
+});
+
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+var lines = [];
+var reader = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+reader.on("line", (line) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  a = lines[0].split(" ");
+  a.forEach((element) => console.log(element));
 });
