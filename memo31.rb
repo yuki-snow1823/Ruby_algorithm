1d4d9ab69160d3f945ec6c86e96c4cafa9e753a0
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-01 23:20:57 +0900

    [add]日時と気温の情報整理

--- memo31.rb
+++ memo31.rb
@@ -12,3 +12,38 @@ rule.each do |b|
 end
 
 p numbers # [2,4]
+
+data = gets.split(" ").map(&:to_i)
+all_days = data[0]
+travel_days = data[1]
+
+info = (1..all_days).map{gets.chomp.split(" ").map(&:to_i)}
+
+combi_info = info.combination(travel_days).to_a
+
+check_arr = []
+answer_arr = []
+combi_info.each do |a|
+    a.each_with_index do |b,c|
+        check_arr << b[0] if b[0] == a[c + 1][0] -1 if a[c + 1] !=nil
+        check_arr << b[0] + 1 if check_arr.count == travel_days - 1
+        answer_arr << a if check_arr.count == travel_days
+    end
+    check_arr = []
+end
+
+continuous_info = answer_arr.uniq!
+
+arr = []
+continuous_info.each do |f|
+    total = 0
+    f.each do |g|
+        total += g[1]
+    end
+    arr << total/travel_days
+end
+
+answer_index = arr.index(arr.min)
+
+puts "#{continuous_info[answer_index].first[0]} #{continuous_info[answer_index].last[0]}"
+
